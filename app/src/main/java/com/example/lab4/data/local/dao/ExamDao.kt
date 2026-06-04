06cb2a0c556faec7cd2f88f714549a0737dd5cf3
elfaknikola <nikola.davidovic@elfak.ni.ac.rs>
2026-06-04 13:44:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab4/data/local/dao/ExamDao.kt
@@ -0,0 +1,19 @@
+package com.example.lab4.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.lab4.data.local.entity.ExamEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExamDao {
+
+    @Query("SELECT * FROM exams ORDER BY id ASC")
+    fun getAllExams(): Flow<List<ExamEntity>>
+
+    //TODO: implement insertExam function
+
+    @Query("SELECT COUNT(*) FROM exams")
+    suspend fun getExamCount(): Int
+}
